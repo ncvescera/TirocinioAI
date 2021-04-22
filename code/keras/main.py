23848752b386d7006cc4ae641701c8dba57fca99
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-04-22 07:26:02 +0000

    Multifilter Keras
    Modificato condice main.py per adattarlo alle nuove esigenze di testing.
    Aggiunta la modalita' File: permette di leggere i dataset da utilizzare da file.
    Ora bisogna sempre specificare il dataset che si vuole usare e non e' piu' scritto
    all'interno del codice.
    
    fix #30

--- code/keras/main.py
+++ code/keras/main.py
@@ -3,24 +3,27 @@ from threading import Thread
 import argparse
 
 
-dataset_dir = '../dataset/imagenet-mini/val'
-aline = '../dataset/ALine'
+# dataset_dir = '../dataset/imagenet-mini/val'
+dataset_dir = ''
 MAX_THREAD_NUMBER = 3       # numero massi di thread che possono essere eseguiti contemporaneamente
 
 
-# funzione utilizzata per il multithreading
-# Testa un dato modello
-#
-# model: modello da testare
-def worker(model, gscale: bool):
-    model.test(dataset_dir, gscale)
+def menu(all=False):
+    """Chiede all'utente di scegliere quali modelli testare (tra quelli disponibili).
 
+        Parameters:
+            all=False (bool): se 'True' ritorna tutti i modelli senza stampare nulla a video.
 
-# permette all'utente di scegliere quali modelli testare (tra quelli disponibili)
-def menu():
+        Return:
+            list[ProtoModel]: lista contenente i modelli scelti dall'utente da testare
+    """
+
     # prende tutti i modelli disponibili
     modelli = models.get_models()
     modelli = list(modelli.values())
+
+    if all:
+        return modelli
 
     print("Questi sono i modelli disponibili: ")
     print()
@@ -47,47 +50,85 @@ def menu():
     return result
 
 
-# scarica tutti i modelli disponibili
-# andrebbe eseguita prima della fase di testing per assicurarsi 
-# che siano presenti tutti i modelli
 def download_all():
+    """Scarica tutti i modelli disponibili
+        Andrebbe eseguita prima della fase di testing per assicurarsi che siano presenti tutti i modelli
+    """
+
     modelli = models.get_models()
 
     for key, modello in modelli.items():
         modello
 
 
-def main(args):
-    # controlla se il dataset scelto e' giusto
-    if args.dataset is not None:
-        if args.dataset == 'ALine':
-            global dataset_dir
-            dataset_dir = aline
+def from_file(file_name: str, modelli, gscale):
+    """Testa i modelli scelti con tutti i dataset presenti nel file.
+        Per ogni dataset crea una cartella dove sposta tutti i dati (file .csv creati dalla fase di testing)
+        ottenuti dal testing.
+        Il file deve contenere una riga per path, tipo:
+            
+            ./path/to/dataset1
+            ../path/to/dataset2
+            /home/path/to/dataset3/
         
-        elif args.dataset == 'ImageNet':
-            pass
-        
-        else:   # se la scelta e' sbagliata interrompe il programma
-            print('Dataset inesistente, controlla meglio !')
-            return
+        Parameters:
+            file_name (str)             : nome del file che contiene tutti i path dei dataset da utilizzare
+            modelli (list[ProtoModel])  : lista con tutti i modelli da testare
+            gscale (bool)               : se 'True' applica il filtro GrayScale alle immagini
+    """
 
-    gscale = args.grayscale
+    # legge tutti i dataset dal file e li mette in una lista
+    datasets = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            to_append = line.strip('\n')
+            to_append = to_append[:-1] if to_append[-1] == '/' else to_append
+            
+            datasets.append(to_append)
 
-    if args.download:
-        print('Download all Models data ...')
-        download_all()
-        return
+    # TODO: questa parte va migliorata per renderla indipendente dal OS ed evitare l'uso specifico di comandi shell
+    import os
+    global dataset_dir
+    
+    for dataset in datasets:
+        dataset_dir = dataset
 
-    modelli = menu()
+        # avvia il testing multithreading o single threading
+        threading_handler(modelli, gscale)
 
-    # avvia la modalità sequenziale
-    if args.nothreads:
+        new_dir = dataset_dir.split('/')[-1]    # prende il nome da dare alla nuova cartella
+
+        # TODO: evitare comandi shell in questo modo !!
+        os.system(f'mkdir {new_dir}')            # crea la cartella del relativo dataset
+        os.system(f'mv *.csv {new_dir}')        # sposta tutti i csv dentro la cartella
+
+
+def threading_handler(modelli, gscale, noThread=False):
+    """Funzione che gestisce il multithreading
+
+        Parameters:
+            modelli (list[ProtoModel])  : lista con tutti i modelli da testare
+            gscale (bool)               : se 'True' applica il filtro GrayScale alle immagini
+            noThread=False (bool)       : se 'True' disabilita il MultiThreading
+    """
+
+    def worker(model, gscale: bool):
+        """Testa un dato Modello.
+
+            Parameters:
+                model (ProtoModel)  : modello da testare
+                gscale (bool)       : se 'True' applica il filtro GrayScale alle immagini
+        """
+
+        model.test(dataset_dir, gscale)
+
+    # esegue il testing in singleThread
+    if noThread:
         for model in modelli:
-            worker(model, gscale)
-    
+                worker(model, gscale)
+        
         return
     
-    # versione multithreading sensata
     ts = []     # lista contenente i thread attivi
     i = 0       # variabile per contare quanti thread sono stati creati
     for model in modelli:
@@ -112,6 +153,27 @@ def main(args):
         t.join()
 
 
+def main(args):
+    # setta il path del dataset
+    global dataset_dir
+    dataset_dir = args.input[:-1] if args.input[-1] == '/' else args.input
+
+    gscale = args.grayscale
+
+    if args.download:
+        print('Download all Models data ...')
+        download_all()
+        return
+
+    modelli = menu(all=args.all)
+
+    if args.file:
+        from_file(dataset_dir, modelli, gscale)
+    
+    else: 
+        threading_handler(modelli, gscale, noThread=args.nothreads)
+
+
 if __name__ == "__main__":
     # creazione del parser
     parser = argparse.ArgumentParser(description="Script per testare alcuni modelli di Image Classification di Keras")
@@ -120,7 +182,10 @@ if __name__ == "__main__":
     parser.add_argument("-g", "--grayscale", help="Applica a tutte le immaigni il filtro GrayScale.", action="store_true")
     parser.add_argument("-d", "--download", help="Scarica i modelli e termina lo script.", action="store_true")
     parser.add_argument("--nothreads", help="Esegue il testing in meniera sequenziale senza multithreading", action="store_true")
-    parser.add_argument("--dataset", type=str, help="Seleziona il dataset con cui testare i modelli.\nScelte: ALine | ImageNet")
+    parser.add_argument("-i", "--input", type=str, help="Path del dataset (o del file) con cui testare i modelli", required=True)
+    parser.add_argument("-a", "--all", help="Testa tutti i modelli disponibili senza stampare il menu di scelta iniziale", action="store_true")
+
+    parser.add_argument("-f", "--file", help="Indica che il path dell'argomento input e' un file e testera' i modelli con ogni path presente nel file.", action="store_true")
 
     # crea gli argomenti da passare alla funzione main
     args = parser.parse_args()
