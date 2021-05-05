02cd5fb862242d319b7c3f6deb64252c99f52381
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-05-05 11:58:42 +0200

    Aggiunto codice per training di DeepUndeeper

---
+++ code/deep_undeeper/training/README.md
@@ -0,0 +1,25 @@
+# DeepUndeeper Model Training
+
+In questa cartella c'e' lo script che ho utilizzato per allenare DeepUndeeper224.
+La shape di input e' passata da 300x300 a 224x224 (formato molto piu' comune tra i modelli preallenati di keras e pytorch).
+
+Di default utilizza 
+
+```
+epochs 	    = 200
+batch_size  = 8
+```
+
+Con la 1080 una batch_size di 16 risultava eccessiva in quanto occupava tutta la VRAM !!
+
+Il dataset usato e' un subset del validation dataset di ImageNet, ho preso le immagini dalla 40000 fino alla 50000 (comprese).
+
+Con gli argmoenti `--epochs` e `--batch` e' possibile specificare relativamente il numero di epoche e il numero di batch:
+
+```
+# default run
+python main.py -e 200 -b 8
+
+# oppure
+python main.py --epochs 200 --batch 8
+```
---
+++ code/deep_undeeper/training/main.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import argparse
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Input, Conv2D, UpSampling2D, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+EPOCHS  = 200   # indica il numero di epoche da utilizzare nella fase di training
+BATCH   = 8     # indica il numero di batch da utilizzare nella fase di training
+
+
+def create_model():
+    """Prepara il modello che analizza le immagini
+        Return:
+            keras.Model: modello della rete
+    """
+
+    x = Input(shape=(224, 224, 3))      # reso input shape standard
+
+    # Encoder
+    e_conv1 = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+    pool1 = MaxPooling2D((2, 2), padding='same')(e_conv1)
+    batchnorm_1 = BatchNormalization()(pool1)
+    e_conv2 = Conv2D(256, (3, 3), activation='relu', padding='same')(batchnorm_1)#32
+    pool2 = MaxPooling2D((2, 2), padding='same')(e_conv2)
+    batchnorm_3 = BatchNormalization()(pool2)
+    e_conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(batchnorm_3)#64
+    h = MaxPooling2D((2, 2), padding='same')(e_conv4)
+
+    # Decoder
+    d_conv1 = Conv2D(512, (3, 3), activation='relu', padding='same')(h)
+    up1 = UpSampling2D((2, 2))(d_conv1)
+    d_conv2 = Conv2D(256, (3, 3), activation='relu', padding='same')(up1)
+    up2 = UpSampling2D((2, 2))(d_conv2)
+    d_conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(up2) # ho dovuto aggiungere padding=same
+    up3 = UpSampling2D((2, 2))(d_conv3)
+    r = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(up3)
+
+    model = Model(x, r)
+    model.compile(optimizer='adam', loss='mse')
+
+    return model
+
+
+def prepare_images(dataset_path: str) -> ImageDataGenerator:
+    # definizione di ImageDataGenerator per caricare in modo efficiente le immagini
+    datagen = ImageDataGenerator(
+            data_format = "channels_last",  # serve per avere un array del tipo (sample, height, width, channels)
+            rescale=1./255                  # normalizzazione (??) delle immagini
+                                            # fa la stessa cosa che con CV2 [(pixel-255)/255]        
+        )
+
+    # carica le immagini dalla cartella del dataset
+    result = datagen.flow_from_directory(
+            dataset_path,                       # path della cartella del dataset
+            target_size=(224, 224),             # (height, width) resize che verrà fatto all'immagine
+            batch_size=BATCH,                   # 16 occupano troppa memoria per la 1080
+            class_mode='input'                  # indica il tipo dell'arry delle classi. 'input' ritorna immagini uguali a quelle di input     
+        )
+
+    return result
+
+
+def main(args):
+    # setto le variabili globali per la fase di training
+    global EPOCHS
+    global BATCH
+
+    BATCH   = args.batch
+    EPOCHS  = args.epochs
+
+    # prendo le immagini
+    imgs = prepare_images('./ilsvrc2012Training')
+
+    # creo il modello
+    deepundeeper = create_model()
+    print(deepundeeper.summary())
+
+    # alleno il modello
+    deepundeeper_history = deepundeeper.fit(imgs, epochs=EPOCHS)
+    
+    # salvo il modello
+    deepundeeper_history.save('deep_undeeper224.model')
+
+    # salvo il grafico
+    plt.plot(deepundeeper_history.epoch, deepundeeper_history.history['loss'])
+    plt.title('Epochs on Training Loss')
+    plt.xlabel('# of Epochs')
+    plt.ylabel('Mean Squared Error')
+    plt.savefig('model_training_chart.png')
+
+
+if __name__ == '__main__':
+     # creazione del parser
+    parser = argparse.ArgumentParser(description="Script per allenare la rete DeepUndeeper224")
+
+    # dichiaro gli argomenti opzionali
+    parser.add_argument(
+        "-b", 
+        "--batch", 
+        type=int, 
+        help="Numero di batch da utilizzare durante la fase di training", default=8
+        )
+
+    parser.add_argument(
+        "-e", 
+        "--epochs", 
+        type=int, 
+        help="Numero di epoche da utilizzare durante la fase di training", default=200
+        )
+
+    # crea gli argomenti da passare alla funzione main
+    args = parser.parse_args()
+    
+    main(args)
